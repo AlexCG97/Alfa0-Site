55c18b421defb245e074ef0a63266c5ae8658fd5
AlexCG97 <ghiurcaalex@gmail.com>
2018-02-22 09:42:43 +0100

    Inserite api controllo schede.

---
+++ API_controlloSchede.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gestioneambulanze";
+
+$usernameinserito = $_POST["Username"];
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+mysqli_query($conn, "SET NAMES 'utf'");
+
+$query = 'SELECT * from chiamate WHERE assegnato = 0 AND macchinaAssegnata = "'.$usernameinserito.'"';
+$result = mysqli_query($conn, $query) or die('error: ' .mysql_error());
+
+if(mysqli_num_rows(($result)) == 1) {
+	echo "presente";
+}
+else{
+	echo "nessuna";
+}
+
+?>
